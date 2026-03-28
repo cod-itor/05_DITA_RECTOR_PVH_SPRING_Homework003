84a81255a9d7d9b4217d47fe5f64b659fe7a0173
Dita rector <rectordita@gmail.com>
2026-03-28 16:35:23 +0700

    Exception Handling

---
+++ src/main/java/com/hrd/springhomework03/Model/Entity/EventAttendee.java
@@ -0,0 +1,12 @@
+//package com.hrd.springhomework03.Model.Entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//public class EventAttendee {
+//    private
+//}
---
+++ src/main/java/com/hrd/springhomework03/Model/Entity/Events.java
@@ -0,0 +1,9 @@
+package com.hrd.springhomework03.Model.Entity;
+
+public class Events {
+    private Long eventId;
+    private String eventName;
+    private String eventDate;
+    private Venue venueId;
+
+}
