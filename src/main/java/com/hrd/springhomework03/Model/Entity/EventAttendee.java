ac2d2ef692a64a13c016e2c445ee6edbc7ab2c85
Dita rector <rectordita@gmail.com>
2026-03-29 10:36:21 +0700

    New classes Entity

--- src/main/java/com/hrd/springhomework03/Model/Entity/EventAttendee.java
+++ src/main/java/com/hrd/springhomework03/Model/Entity/EventAttendee.java
@@ -1,12 +1,14 @@
-//package com.hrd.springhomework03.Model.Entity;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@AllArgsConstructor
-//@Data
-//@NoArgsConstructor
-//public class EventAttendee {
-//    private
-//}
+package com.hrd.springhomework03.Model.Entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventAttendee {
+    private Events eventId;
+    private Attendees attendeeId;
+}
